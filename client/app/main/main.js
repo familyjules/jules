2ca31880dd00afd11ef29542dfdbf20e1e5f6f3d
Tyler Lambe <tylambe@live.com>
2015-04-29 19:34:00 -0500

    Deployed and ready to be developed

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('julesApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/jules-dev'
+  },
+
+  seedDB: true
+};
