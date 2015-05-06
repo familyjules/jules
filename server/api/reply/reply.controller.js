9ece737882f198c69495489a20e75166c01dcf7e
SaintEmbers <quantum108@gmail.com>
2015-05-06 15:07:49 -0500

    work on text ratings

--- server/api/reply/reply.controller.js
+++ server/api/reply/reply.controller.js
@@ -2,10 +2,10 @@
 var mongoConfig = require('../../config/environment')
 var mongo = require('mongodb').MongoClient
 var DB;
+var request = require('request');
 var _ = require('lodash');
 var accountSid = 'AC0d4f667900e2a6fea95046313f539958'; 
 var authToken = '8dd9c7e404b9b17113030ae34db27443'; 
-//require the Twilio module and create a REST client 
 var client = require('twilio')(accountSid, authToken); 
 var watson = require('watson-developer-cloud');
 
@@ -17,25 +17,82 @@ var question_and_answer_healthcare = watson.question_and_answer({
 });
 
 var responses = ['I must admit, I really prefer questions that start with "what"','You can ask a better question than that!', "Who is Jules? I'll take potent potables for 500, Alex",'Please rephrase the nature of the medical emergency',"I'm sorry. My responses are limited. You must ask the right questions."]
+mongo.connect(mongoConfig.mongo.uri, function (err, db){
+  if(err){
+    console.log(err)
+  }
+  else{
+    DB = db;
+    console.log('connected to mongo')
+}
+})
 
- 
-// Get list of replys
 exports.index = function(req, res){
-  //req.body should contain all of the information from the incoming messages
   var question = req.body.Body
   var city = req.body.FromCity
   var state = req.body.FromState
   var zipcode = req.body.FromZip
+  var thisPhone = req.body.From
+  // var body = DB.collection('requests').find({city: 'Austin'});
+  //   body.on('data', function(datum){
+  //   console.log(datum)
+  // })
+    if(question.length === 1 && (question - 0) == question){
+      // send rating to watson
+      // body.feedback = 1
+      app.post('/feedback', function(req, res) {
+      var options = {
+        url: 'https://gateway.watsonplatform.net/question-and-answer-beta/api/v1/feedback',
+        method: 'PUT',
+        json: true,
+        body: 1,
+        auth: {
+          user: "8a2c0b68-8e22-4ed4-931f-7f10c7e3b339",
+          pass: "fyMSE9bnwewL",
+          sendImmediately: true
+        }
+      };
+      request(options, function(err, response) {
+        if (response.statusCode === 200 || response.statusCode == 201) {
+          console.log('feedback accepted');
+          client.messages.create({
+          to: "+15105990762",
+          from: "+14153196727",
+          body: 'thanks for the feedback!'
+        }, function(err, message){
+          console.log(err);
+          res.status(200);
+          res.end();
+        })
+        } else {
+          console.log('feedback error, missing parameter?');
+        }
+        res.end();
+      });
+      })
+  } else{  
   question_and_answer_healthcare.ask({text: question}, function (err, response) {
     var answer = response[0].question.evidencelist[0].text
     if(response[0].question.evidencelist[0].value > .3){
+      DB.collection('requests').insert({
+        channel: 'sms', 
+        phoneNumber: phoneNumber, 
+        city: city, 
+        state: state, 
+        zipcode: zipcode, 
+        info: {
+          questionId: response[0].question.id,
+          questionText: response[0].question.questionText,
+          answerId: response[0].question.answers[0].id,
+          answerText: response[0].question.answers[0].text,
+          confidence: response[0].question.answers[0].confidence
+        }
       client.messages.create({
         to: "+15105990762",
         from: "+14153196727",
-        body: answer
+        body: answer + '  How would you rate this answer? 0 for bad, 1 for neutral, 2 for good'
       }, function(err, message){
         console.log(err);
-        DB.collection('requests').insert({question : question, answer: answer, channel: 'sms', city: city, state: state, zipcode: zipcode})
         res.status(200);
         res.end();
       })
@@ -50,8 +107,7 @@ exports.index = function(req, res){
         res.status(200);
         res.end();
       })
-      }
+    }
   });
-
-
+  }
 };
