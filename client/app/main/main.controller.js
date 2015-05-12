260aa935d3e1a7705c0ffe9f0fdb05ab7eae912f
Claire McEnery <brian.m.small@gmail.com>
2015-05-12 14:37:59 -0500

    added data visualization stuff post refactor

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,8 +4,8 @@ angular.module('julesApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
 
   });
