2a345d3c76b215c12fde830f218c82be0e45bad7
Samuel Thomas <samuelfullerthomas@gmail.com>
2015-05-15 16:24:21 -0500

    Data visualization and FAQ

--- server/api/d3/usa/d3.usa.controller.js
+++ server/api/d3/usa/d3.usa.controller.js
@@ -8,16 +8,29 @@ var _ = require('lodash'),
 // Get list of citydatas
 exports.index = function(req, res) {
   var db = pmongo(config.mongo.uri);
-  var retObj = {};
-  db.collection('requests').distinct("state").then(function(states){
-
-    var statePromises = states.map(function (state) {
-      return db.collection('requests').count({"state": state})
+  var retArr = [];
+  Promise(db.collection('requests').find({"state" : {$ne : null}}).on('data', function(states){
+      retArr.push(states)
+    })).then(function () {
+      res.json(retArr);
     });
 
-    Promise.all(statePromises).then(function (results) {
-      res.json(results);
-    });
-
-  });
 };
+// 'use strict';
+
+// var _ = require('lodash'),
+//         config = require('../../.././config/environment'), 
+//         mongo = require('mongodb').MongoClient,
+//         request = require('request'),
+//         DB;
+
+// // Get list of citydatas
+// mongo.connect(config.mongo.uri, function (err, db){
+//   DB = db;
+// });
+// exports.index = function(req, res) {
+//   var responseArr = [];
+//   var states = DB.collection('requests').find({"state" : {$ne : null}});
+//   states.on('data', function(st){
+//       responseArr.push(st)
+//     }).then()
