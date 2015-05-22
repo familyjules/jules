128173b996456e3fcd870882fe6e5f94130e69c1
Samuel Thomas <samuelfullerthomas@gmail.com>
2015-05-22 12:25:16 -0500

    working on visualizations

--- client/app/dataVisualizer/dataVisualizer.controller.js
+++ client/app/dataVisualizer/dataVisualizer.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('julesApp')
-  .controller('DataVisualizerCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('DataVisualizerCtrl', function ($scope, $http) {
+    $scope.message = 'goodbye';
+    $scope.data;
+    $scope.getData = function() {
+      console.log('in data');
+      $http({
+      method: 'GET',
+      url: '/api/d3/usa',
+    })
+    	.then(function(res) {
+      		$scope.data = res.data
+      		window.data = res.data
+      		console.log('got response')
+    	})
+  	}
+    $scope.getData()
   });
